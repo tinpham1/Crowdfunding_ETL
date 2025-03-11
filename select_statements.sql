c4d31711b2fbc494f4fdd6a344d801f57eda90b4
tinpham1 <flashrabbit@gmail.com>
2025-03-11 15:50:45 -0600

    Adding SQL select statements and DBD

---
+++ select_statements.sql
@@ -0,0 +1,19 @@
+SELECT table_name 
+FROM information_schema.tables 
+WHERE table_schema = 'public';
+
+SELECT *
+FROM campaign
+LIMIT 10;
+
+SELECT *
+FROM category
+LIMIT 10;
+
+SELECT *
+FROM contacts
+LIMIT 10;
+
+SELECT *
+FROM subcategory
+LIMIT 10;
